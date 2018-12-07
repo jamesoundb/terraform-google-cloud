2dc48cb8faa3e3b63f8aa1161b91baee753967ea
Nick Apostolakis <nickapos@oncrete.gr>
2018-12-07 10:15:25 +0000

    adding gcloud env file

---
+++ gcloud-env.sh
@@ -0,0 +1,5 @@
+export TF_VAR_org_id=YOUR_ORG_ID
+export TF_VAR_billing_account=YOUR_BILLING_ACCOUNT_ID
+export TF_ADMIN=${USER}-terraform-admin
+export
+TF_CREDS=/home/nickapos/.config/gcloud/legacy_credentials/nickapos@gmail.com/adc.json
